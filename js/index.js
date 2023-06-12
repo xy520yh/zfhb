7a3aeff2823c33dd5aa1f95adeb1475587e5fa83
unknown <705719648@qq.com>
2023-06-12 19:00:47 +0800

    2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function rePosition(){
     var width = document.documentElement.clientWidth || document.body.clientWidth;
-    var ratio = width/750;
-    if(width<=750){
+    var ratio = width/1334;
+    if(width<=1334){
         document.getElementsByTagName('html')[0].style.fontSize=100*ratio+"px";
     }else{
         document.getElementsByTagName('html')[0].style.fontSize="100px";
